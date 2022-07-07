a0b1c98ed3c4bca8288e9ba49d820dd69ac06372
Storiofmylife <79773725+storiofmylife@users.noreply.github.com>
2022-07-07 15:28:45 -0400

    idk

---
+++ src/main/java/net/storiofmylife/dct/DecorativeCraftingTablesClient.java
@@ -0,0 +1,10 @@
+package net.storiofmylife.dct;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DecorativeCraftingTablesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/storiofmylife/dct/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.storiofmylife.dct.mixin;
+
+import net.storiofmylife.dct.DecorativeCraftingTables;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		DecorativeCraftingTables.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
